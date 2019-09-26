132e9672d00d816c27217e598fbd6923bb26febd
Sasila Hapuarachchi <sasilaah@gmail.com>
2019-09-26 15:13:39 -0400

    Initial Commit

---
+++ HealthEye/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HealthEye
+//
+//  Created by Sasila Hapuarachchi on 2019-09-26.
+//  Copyright © 2019 Sasila Hapuarachchi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HealthEye/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HealthEye
+//
+//  Created by Sasila Hapuarachchi on 2019-09-26.
+//  Copyright © 2019 Sasila Hapuarachchi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
